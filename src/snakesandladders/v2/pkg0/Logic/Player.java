e92859feab7d411b51e105a79de7c3e9dc9cf104
Sokratis Vogiatzakis <sokvogiatzakis@gmail.com>
2017-05-24 16:39:43 +0300

    //soc code comments

--- src/snakesandladders/v2/pkg0/Logic/Player.java
+++ src/snakesandladders/v2/pkg0/Logic/Player.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//A TEST
 package snakesandladders.v2.pkg0.Logic;
 
 import javax.swing.JLabel;
